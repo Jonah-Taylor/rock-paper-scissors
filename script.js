12f248d2ee824fc160f2a69b4d3b47ebcf3b7c2c
Jonah Taylor <jt3438@drexel.edu>
2025-04-30 13:52:56 -0400

    A working game of rock paper scissors (clearer results desired)

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 
-
 let humanScore = 0;
 let computerScore = 0;
 
-let humanScoreText = document.querySelector(".humanScore .score");
-let computerScoreText = document.querySelector(".computerScore .score");
+let humanScoreText = document.querySelector("#hs");
+let computerScoreText = document.querySelector("#cs");
 
+let result = document.querySelector(".result");
 
-
-console.log("hello world");
 
 function playRound(humanChoice, computerChoice) {
     
-    wonGame = false;
+    let wonGame = false;
 
     if (humanChoice === "rock" && computerChoice === "scissors") {
         wonGame = true;
@@ -21,17 +19,17 @@ function playRound(humanChoice, computerChoice) {
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         wonGame = true;
     }
-
+    
     if (humanChoice === computerChoice) {
-        console.log(`Tie ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} ties with ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+        result.textContent = (`Tie! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} ties with ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
     } else if (wonGame) {
         humanScore += 1;
         humanScoreText.textContent = humanScore;
-        console.log(`You Won! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+        result.textContent = (`Humans Point! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
     } else {
         computerScore += 1;
         computerScoreText.textContent = computerScore;
-        console.log(`You Lost! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} loses to ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+        result.textContent = (`Computer's Point! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} loses to ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
     }
 }
 
@@ -52,29 +50,21 @@ function getComputerChoice() {
     return "error"
 }
 
-function getHumanChoice() {
-    return prompt("Rock, paper, or scissors?").toLowerCase();
-}
 
-function playGame() {
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    if (humanScore > computerScore) {
-        console.log("The human wins!");
-    } else if (humanScore < computerScore) {
-        console.log("The computer wins!");
-    } else {
-        console.log("It's a tie!");
-        }
-}
-
-let buttons = document.querySelectorAll("buttons");
+let buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        playRound(event.target.value, getComputerChoice());
+        let cc = getComputerChoice()
+        playRound(event.target.textContent.toLowerCase(), cc);
+        if (humanScore === 5 || computerScore === 5) {
+            if (humanScore === 5) {
+                result.textContent = (`The human wins with a score of ${humanScore} against ${computerScore}!`);
+            } else {
+                result.textContent = (`The computer wins with a score of ${computerScore} against ${humanScore}!`);
+            }
+            humanScore = 0;
+            computerScore = 0;
+        }
     });
 });
